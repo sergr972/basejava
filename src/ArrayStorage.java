8efdc88ea70150960a2df845d9df3cab5ca0e1d3
sergr972 <sergr972@gmail.com>
2023-01-07 13:18:29 +0300

    ArrayStorage на проверку

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,61 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            if (storage[i] == null) {
+                size--;
+            }
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = resume;
+                size = i + 1;
+                return;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = null;
+            }
+        }
+        for (int j = 0; j < size; j++) {
+            if (storage[j] == null) {
+                storage[j] = storage[j + 1];
+                storage[j + 1] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
