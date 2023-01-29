e21463e51df7eb9e63cf54be37c4c93f673ff1cd
sergr972 <sergr972@gmail.com>
2023-01-29 22:18:33 +0300

    добавлен test для SortedArrayStorage

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -15,6 +17,34 @@ public abstract class AbstractArrayStorage implements Storage {
         return size;
     }
 
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Резюме " + r.getUuid() + " нет!");
+        } else {
+            storage[index] = r;
+            System.out.println("Резюме " + r.getUuid() + " обновлено.");
+        }
+    }
+
+    public void save(Resume r) {
+        String uuid = r.getUuid();
+        if (getIndex(uuid) != -1) {
+            System.out.println("Резюме " + r.getUuid() + " уже есть!!!");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("База заполнена!!!");
+        } else {
+            storage[size] = r;
+            size++;
+            System.out.println("Резюме " + uuid + " добавлено!");
+        }
+    }
+
     public Resume get(String uuid) {
         int index = getIndex(uuid);
         if (index == -1) {
@@ -22,6 +52,22 @@ public abstract class AbstractArrayStorage implements Storage {
             return null;
         }
         return storage[index];
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Резюме " + uuid + " нет!");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+            System.out.println("Резюме " + uuid + " успешно удалено!");
+        }
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     protected abstract int getIndex(String uuid);
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,69 +1,11 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.model.Resume;
-
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index == -1) {
-            System.out.println("Резюме " + r.getUuid() + " нет!");
-        } else {
-            storage[index] = r;
-            System.out.println("Резюме " + r.getUuid() + " обновлено.");
-        }
-    }
-
-    public void save(Resume r) {
-        if (getIndex(r.getUuid()) != -1) {
-            System.out.println("Резюме " + r.getUuid() + " уже есть!!!");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("База заполнена!!!");
-        } else {
-            storage[size] = r;
-            size++;
-            System.out.println("Резюме " + r.getUuid() + " добавлено!");
-        }
-    }
-
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Резюме " + uuid + " нет!");
-            return null;
-        }
-        return storage[index];
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Резюме " + uuid + " нет!");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-            System.out.println("Резюме " + uuid + " успешно удалено!");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
-
+    @Override
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
