6577c19226e41d23efbe9fbd72813102d6086fd6
sergr972 <sergr972@gmail.com>
2023-02-16 10:40:34 +0300

    Reformat MainTest

--- src/com/urise/webapp/MainTestSortedArrayStorage.java
+++ src/com/urise/webapp/MainTestSortedArrayStorage.java
@@ -22,25 +22,25 @@ public class MainTestSortedArrayStorage {
         ARRAY_STORAGE.save(r3);
         System.out.println("\nСохранить резюме r2");
         ARRAY_STORAGE.save(r2);
-        System.out.println("\nСохранить резюме r2");
-        ARRAY_STORAGE.save(r2);
+//        System.out.println("\nСохранить резюме r2");
+//        ARRAY_STORAGE.save(r2);
 
         System.out.println("\nВернуть резюме r2");
         System.out.println("Get r2: " + ARRAY_STORAGE.get(r2.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        System.out.println("\nВернуть резюме dummy");
-        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+//        System.out.println("\nВернуть резюме dummy");
+//        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
-        System.out.println("\nОбновить резюме r4");
-        ARRAY_STORAGE.update(r4);
+//        System.out.println("\nОбновить резюме r4");
+//        ARRAY_STORAGE.update(r4);
         System.out.println("\nОбновить резюме r1");
         ARRAY_STORAGE.update(r1);
 
         printAll();
 
-        System.out.println("\nУдалить резюме r4");
-        ARRAY_STORAGE.delete(r4.getUuid());
+//        System.out.println("\nУдалить резюме r4");
+//        ARRAY_STORAGE.delete(r4.getUuid());
         System.out.println("\nУдалить резюме r2");
         ARRAY_STORAGE.delete(r2.getUuid());
 
