beeb0bd3c304de457984cfb9db75d6e857342750
sergr972 <sergr972@gmail.com>
2023-02-18 21:25:37 +0300

    MapStorage -> MapUuidStorage

--- src/com/urise/webapp/storage/MapUuidStorage.java
+++ src/com/urise/webapp/storage/MapUuidStorage.java
@@ -4,7 +4,7 @@ import com.urise.webapp.model.Resume;
 
 import java.util.*;
 
-public class MapStorage extends AbstractStorage {
+public class MapUuidStorage extends AbstractStorage {
     private final Map<String, Resume> map = new HashMap<>();
 
 
