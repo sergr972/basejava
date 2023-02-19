9cc89810fa05958505a67857260e5a8ac4cc578c
sergr972 <sergr972@gmail.com>
2023-02-19 22:10:13 +0300

    reformat

---
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,29 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest{
+    protected AbstractArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+
+    @Test
+    public void saveOverflow() {
+        Assertions.assertThrows(StorageException.class, () -> {
+            try {
+                storage.clear();
+                for (int i = 0; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                    storage.save(new Resume(UUID.randomUUID().toString(), "name"));
+                }
+            } catch (StorageException e) {
+                Assertions.fail("переполнение произошло раньше времени");
+            }
+            storage.save(new Resume(UUID.randomUUID().toString(), "name"));
+        });
+    }
+}
--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -1,30 +1,8 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.exception.StorageException;
-import com.urise.webapp.model.Resume;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.util.UUID;
-
-public class ArrayStorageTest extends AbstractStorageTest {
+public class ArrayStorageTest extends AbstractArrayStorageTest {
 
     public ArrayStorageTest() {
         super(new ArrayStorage());
-    }
-
-    @Test
-    public void saveOverflow() {
-        Assertions.assertThrows(StorageException.class, () -> {
-            try {
-                storage.clear();
-                for (int i = 0; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
-                    storage.save(new Resume(UUID.randomUUID().toString(), "name"));
-                }
-            } catch (StorageException e) {
-                Assertions.fail("переполнение произошло раньше времени");
-            }
-            storage.save(new Resume(UUID.randomUUID().toString(), "name"));
-        });
     }
 }
