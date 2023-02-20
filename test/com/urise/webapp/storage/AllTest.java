16e460faf64127eb1faa9397414d9e07c1c238bf
sergr972 <sergr972@gmail.com>
2023-02-20 15:42:12 +0300

    pom

--- test/com/urise/webapp/storage/AllTest.java
+++ test/com/urise/webapp/storage/AllTest.java
@@ -1,9 +1,12 @@
 package com.urise.webapp.storage;
 
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
+//@RunWith(org.junit.platform.runner.JUnitPlatform.class)
+@SuiteDisplayName("AllTest")
+//@SelectPackages(com.urise.webapp.storage)
+//@RunWith(Suite.class)
 @Suite.SuiteClasses({
         ArrayStorageTest.class,
         SortedArrayStorageTest.class,
