0f6f86ec4756348d84ca605c59a03f4e2e379b30
sergr972 <sergr972@gmail.com>
2023-02-20 13:59:07 +0300

    update gitignore

--- test/com/urise/webapp/storage/AllTest.java
+++ test/com/urise/webapp/storage/AllTest.java
@@ -4,12 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class,
-                     ListStorageTest.class,
-                     MapResumeStorageTest.class,
-                     MapUuidStorageTest.class,
-                     SortedArrayStorageTest.class
-} )
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapUuidStorageTest.class,
+        MapResumeStorageTest.class
+})
 
 public class AllTest {
 }
