dc71d786a4c8577f2a77f6f05093cc4d41870b5b
sergr972 <sergr972@gmail.com>
2023-02-20 22:45:54 +0300

    reformat

--- test/com/urise/webapp/storage/AllTest.java
+++ test/com/urise/webapp/storage/AllTest.java
@@ -1,12 +1,9 @@
 package com.urise.webapp.storage;
 
-import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-//@RunWith(org.junit.platform.runner.JUnitPlatform.class)
-@SuiteDisplayName("AllTest")
-//@SelectPackages(com.urise.webapp.storage)
-//@RunWith(Suite.class)
+@RunWith(Suite.class)
 @Suite.SuiteClasses({
         ArrayStorageTest.class,
         SortedArrayStorageTest.class,
