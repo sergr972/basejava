2ceadaa3f5af0ef88807c771d4a9f921ff4bf235
sergr972 <sergr972@gmail.com>
2023-02-20 10:16:57 +0300

    reformat

--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -1,8 +1,30 @@
 package com.urise.webapp.storage;
 
-public class ArrayStorageTest extends AbstractArrayStorageTest {
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class ArrayStorageTest extends AbstractStorageTest {
 
     public ArrayStorageTest() {
         super(new ArrayStorage());
+    }
+
+    @Test
+    public void saveOverflow() {
+        Assertions.assertThrows(StorageException.class, () -> {
+            try {
+                storage.clear();
+                for (int i = 0; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                    storage.save(new Resume(UUID.randomUUID().toString(), "name"));
+                }
+            } catch (StorageException e) {
+                Assertions.fail("переполнение произошло раньше времени");
+            }
+            storage.save(new Resume(UUID.randomUUID().toString(), "name"));
+        });
     }
 }
--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -1,8 +1,30 @@
 package com.urise.webapp.storage;
 
-public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class SortedArrayStorageTest extends AbstractStorageTest {
 
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
+    }
+
+    @Test
+    public void saveOverflow() {
+        Assertions.assertThrows(StorageException.class, () -> {
+            try {
+                storage.clear();
+                for (int i = 0; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                    storage.save(new Resume(UUID.randomUUID().toString(), "name"));
+                }
+            } catch (StorageException e) {
+                Assertions.fail("переполнение произошло раньше времени");
+            }
+            storage.save(new Resume(UUID.randomUUID().toString(), "name"));
+        });
     }
 }
