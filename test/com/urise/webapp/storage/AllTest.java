ebe1c6e91a07914dc67babb6d0f56f902292b32c
sergr972 <sergr972@gmail.com>
2023-02-21 11:02:14 +0300

    add AllTest

--- test/com/urise/webapp/storage/AllTest.java
+++ test/com/urise/webapp/storage/AllTest.java
@@ -1,10 +1,10 @@
 package com.urise.webapp.storage;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
+@Suite
+@SelectClasses({
         ArrayStorageTest.class,
         SortedArrayStorageTest.class,
         ListStorageTest.class,
