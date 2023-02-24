a12a28bcf718a23de62be335ce2526237b01ba10
sergr972 <sergr972@gmail.com>
2023-02-24 19:34:48 +0300

    COMPARATOR to constant

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
+    private static final Comparator<Resume> COMPARATOR = Comparator.comparing(Resume::getFullName)
+            .thenComparing(Resume::getUuid);
     private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     protected abstract SK getSearchKey(String uuid);
@@ -53,9 +55,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     public List<Resume> getAllSorted() {
         LOG.info("getAllSorted");
         List<Resume> list = doGetAll();
-        Comparator<Resume> comparator = Comparator.comparing(Resume::getFullName)
-                .thenComparing(Resume::getUuid);
-        list.sort(comparator);
+        list.sort(COMPARATOR);
         return list;
     }
 
