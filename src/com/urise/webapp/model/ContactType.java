5b4688d97ebf40b07c8f7a2a4de8ec4b6e9a531d
sergr972 <sergr972@gmail.com>
2023-02-27 22:50:27 +0300

    add ContactType

---
+++ src/com/urise/webapp/model/ContactType.java
@@ -0,0 +1,21 @@
+package com.urise.webapp.model;
+
+public enum ContactType {
+    MOBILE_PHONE("моб. телефон"),
+    SKYPE("Skype"),
+    MAIL("Почта"),
+    LINKEDIN("Профиль LinkedIn"),
+    GITHUB("Профиль GitHub"),
+    STATCKOVERFLOW("Профиль Stackoverflow"),
+    HOME_PAGE("Домашняя страница");
+
+    private final String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
